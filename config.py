8821ee44b603bda6434d1b71079804caa2b8e415
Fireluci <120563797+Fireluci@users.noreply.github.com>
2024-07-20 14:38:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,27 +10,27 @@ id_pattern = re.compile(r'^.\d+$')
 
 class Config(object):
     # digital_botz client config
-    API_ID = os.environ.get("API_ID", "")
-    API_HASH = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+    API_ID = os.environ.get("API_ID", "24314601")
+    API_HASH = os.environ.get("API_HASH", "ede341e2d490a0fad5469866dedf8a95")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6498608614:AAFu84eh-eQCn8l75GDcbQ9xblRHkGhBCFg") 
 
     # premium account string session required 😢 
     STRING_SESSION = os.environ.get("STRING_SESSION", "")
     
     # database config
     DB_NAME = os.environ.get("DB_NAME","Digital_Rename_Bot")     
-    DB_URL = os.environ.get("DB_URL","")
+    DB_URL = os.environ.get("DB_URL","mongodb+srv://rename:rename@rename.s2n5fka.mongodb.net/?retryWrites=true&w=majority&appName=rename")
  
     # other configs
     RKN_PIC = os.environ.get("RKN_PIC", "https://telegra.ph/file/b746aadfe59959eb76f59.jpg")
-    ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '6705898491').split()]
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002123429361"))
+    ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '1058015838').split()]
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001652564383"))
 
     #force subs
     try:
         FORCE_SUB = int(os.environ.get("FORCE_SUB", "")) 
     except:
-        FORCE_SUB = os.environ.get("FORCE_SUB", "Digital_Botz")
+        FORCE_SUB = os.environ.get("FORCE_SUB", "")
         
     # wes response configuration     
     PORT = int(os.environ.get("PORT", "8080"))
